f11697b7d020f293226eb7112563934e7e5cd1e5
hh.lohmann <hh.lohmann@yahoo.de>
2014-01-29 12:09:23 +0100

    Created: German lang.php
    #What: created German lang.php
    #Why: usage for German users
    #Note: translated only strings that seem to be in use

---
+++ lang/de/lang.php
@@ -0,0 +1,27 @@
+<?php
+
+$lang['insert_after'] = 'Zeile darunter einfügen';
+$lang['insert_before'] = 'Zeile darüber einfügen';
+$lang['edit'] = 'Bearbeiten';
+$lang['remove'] = 'Zeile löschen';
+$lang['insert_col_left'] = 'Insert column left';
+$lang['insert_col_right'] = 'Insert column right';
+$lang['mark_row_as_header'] = 'Switch this row to header row';
+$lang['mark_col_as_header'] = 'Switch this column to header column';
+$lang['mark_cell_as_header'] = 'Switch this cell to header cell';
+
+$lang['mark_row_as_cell'] = 'Switch this row to normal cells row';
+$lang['mark_col_as_cell'] = 'Switch this column to normal cells column';
+$lang['mark_cell_as_cell'] = 'Switch this header cell to normal cell';
+
+$lang['show_merged_rows'] = 'There are %d more megred cells. Click here to unmerge.';
+
+$lang['db_error'] = 'Error: cannot open file to read/write. Check your permissions and try again.';
+$lang['correct'] = 'correct';
+$lang['upload_file'] = 'upload file';
+$lang['lock_notify'] = 'Diese Seite ist aktuell von %u geöffnet und kann deshalb nicht bearbeitet werden. Bitte warten, bis %u die Seite schließt (der automatische Bearbeitungsschutz endet am / um %t.';
+$lang['unlock_notify'] = 'Der Bearbeitungsschutz für die Seite ist abgelaufen! Seite neu laden, um Tabellen zu bearbeiten.';
+
+$lang['summary_remove'] = 'Tabellenzeile gelöscht:';
+$lang['summary_add'] = 'Tabellenzeile hinzugefügt:';
+$lang['summary_edit'] = 'Tabellenzeile %o geändert nach %n';
